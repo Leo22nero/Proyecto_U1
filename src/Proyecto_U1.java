38250b0a21935ff8f5d739dc9591ba1751434753
s24030124-lgtm <s24030124@itsch.edu.mx>
2026-03-12 22:31:09 -0600

    Update Proyecto_U1.java
    cambio de tamaño de la ventana

--- src/Proyecto_U1.java
+++ src/Proyecto_U1.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 void main() {
     Ventana ventana = new Ventana();
     ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    ventana.setSize(600,700);
+    ventana.setSize(800,500);
     ventana.setVisible(true);
     ventana.setLocationRelativeTo(null);
     ventana.setResizable(false);
