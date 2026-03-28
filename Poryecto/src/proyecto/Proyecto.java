6d045abb3d75f22957a5196a42026bb40d5c3ccd
estra_pxwmdkq <leo22nero@yahoo.com>
2026-03-28 09:16:37 -0600

    Cambios de teclas Atajo

--- Poryecto/src/proyecto/Proyecto.java
+++ Poryecto/src/proyecto/Proyecto.java
@@ -1,24 +1,28 @@
 
 package proyecto;
-
+import com.formdev.flatlaf.FlatDarkLaf;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 
 
 public class Proyecto {
 
- 
+
     public static void main(String[] args) {
-          SwingUtilities.invokeLater(()->{
-            
+        FlatDarkLaf.setup();
+        SwingUtilities.invokeLater(()->{
+
             Ventana ventana = new Ventana();
             ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            ventana.setSize(700, 600);
+            ventana.setSize(1000, 700);
             ventana.setLocationRelativeTo(null);
             ventana.setResizable(false);
             ventana.setVisible(true);
-            
-    });
+            new Ventana().setVisible(true);
+
+        });
     }
-    
+
 }
+
